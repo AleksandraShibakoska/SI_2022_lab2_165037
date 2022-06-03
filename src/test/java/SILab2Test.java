9d74249a9ef83794c48494c899f6474e1c22625a
AleksandraShibakoska <63558400+AleksandraShibakoska@users.noreply.github.com>
2022-06-04 00:46:12 +0200

    Add files via upload

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SILab2Test {
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
